ce2d420f222c4d9f653027c8ad8029dbc79a4368
sravya sangadi <sravyasangadi@gmail.com>
2026-07-15 05:28:42 +0530

    variableexample3
    in this i learned how boolean variables are stored 
    just i try small program by using boolean variable!!

---
+++ variablesexample3.java
@@ -0,0 +1,7 @@
+public class variablesexample3 {
+    public static void main(String[] args){
+        boolean passed = true;
+        System.out.println("he is passed:");
+        System.out.println(passed);
+    }
+}
