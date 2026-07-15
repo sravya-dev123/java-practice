915ef7bfb27a7eaff59c05b4ddebc318d6b4e977
sravya sangadi <sravyasangadi@gmail.com>
2026-07-15 05:40:46 +0530

    variable example4
    i learned variable by using char type, small creativity make me happy 
    small archeivement.

---
+++ variablesexample4.java
@@ -0,0 +1,17 @@
+class Variablesexample{
+    public static void main(String[] args){
+        char student='S';
+        char student1='R';
+        char student2='A';
+        char student3='V';
+        char student4='Y';
+        char student5='A';
+
+        System.out.println(student);
+         System.out.println(student1);
+          System.out.println(student2);
+           System.out.println(student3);
+            System.out.println(student4);
+             System.out.println(student5);
+    }
+}
