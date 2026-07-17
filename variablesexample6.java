3c59ab0c4e19f1213143c21d625ad8c922e36c0e
sravya sangadi <sravyasangadi@gmail.com>
2026-07-17 07:00:46 +0530

    print variables
    i learned print variablesjust i practice it,because of learning new things day practicing.

---
+++ variablesexample6.java
@@ -0,0 +1,10 @@
+public class variablesexample6{
+public static void main(String[] args) {
+    String a,b,c;
+     a="sravya";
+     b="hello";
+     c="how are you";
+    System.out.println("the sum is:" + b + a );
+    System.out.println("the sum is:" + (a + c));
+}
+}
