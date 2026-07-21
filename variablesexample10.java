a63dc9996e6fcdb06bed861a2c285e5caadbeb0a
sravya sangadi <sravyasangadi@gmail.com>
2026-07-21 20:41:20 +0530

    print variables 4
     Java Variable
    
    This repository contains a simple Java program demonstrating the use of text multiple and addition variables. It explains how multiple text and addition text variables are declared inside a class, how they belong to objects, and how they can be accessed using object references.
    
     Topics Covered
    - multiple and addition Variables

---
+++ variablesexample10.java
@@ -0,0 +1,8 @@
+public class variablesexample10 {
+    public static void main(String[] args) {
+    int x = 28;
+    int y = 38;
+    System.out.println("the sum is "+ x + y);
+    System.out.println("the sum is " + (x + y));
+    }
+}
