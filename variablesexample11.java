28e6fff3eb0a70443b3111f05d191070e13b4f89
sravya sangadi <sravyasangadi@gmail.com>
2026-07-22 04:58:51 +0530

    print variables4
    today i used these all by using print variables and created small program
    Java Variables Practice 
    This program demonstrates:
     Integer variables
     Float variables
     Boolean variables
     Modulus (%) operator
     Basic output using System.out.println()
    
    I'm currently learning Java fundamentals and practicing variables step by step.

---
+++ variablesexample11.java
@@ -0,0 +1,26 @@
+public class variablesexample11 {
+    String name = " sravya";
+public static void main(String[] args) {
+    int a=2343, b=432187;
+    System.out.println(a % b);
+    {
+    float studentmarks= 8.9f ;
+    float studentmarks1 = 2.0f;
+
+    if (studentmarks >= 1.9f){
+    System.out.println("he is passed:" + studentmarks);
+    }
+        else
+    {
+   if (studentmarks <= 3.0f)
+    System.out.println("he is not passed:"+studentmarks1);
+    }
+boolean memo=true;
+System.out.println("she gave memo card:" + memo);
+
+variablesexample11 studentname=new variablesexample11();
+System.out.println(studentname.name);
+ }
+}   
+}
+
