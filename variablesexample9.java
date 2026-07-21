feb78518585fd7a216bcc225acf24b90eee0d327
sravya sangadi <sravyasangadi@gmail.com>
2026-07-21 20:28:50 +0530

    print variable 3
    today i learned instance variable(non static field) # Java Instance Variable
    
    This repository contains a simple Java program demonstrating the use of instance variables. It explains how instance variables are declared inside a class, how they belong to objects, and how they can be accessed using object references.
    
     Topics Covered
    - Instance Variables
    - Object Creation
    - Accessing Instance Variables

---
+++ variablesexample9.java
@@ -0,0 +1,7 @@
+public class variablesexample9 {
+    int id = 101;
+    public static void main(String[] args) {
+        variablesexample9 Student = new variablesexample9();
+    System.out.println(Student.id); 
+    }
+}
