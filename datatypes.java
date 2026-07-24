34015f50fedc6bfae5538d80a769bf1f264a0644
sravya sangadi <sravyasangadi@gmail.com>
2026-07-24 16:11:49 +0530

    datatypes
    today i learned how the datatypes values are stored in bytes and bit
    -- float
    -- integer
    -- character
    -- boolean
    -- string
    -- long
    -- double
    --short

---
+++ datatypes.java
@@ -0,0 +1,27 @@
+public class datatypes {
+public static void main(String[] args) {
+//float can store the 4 bytes 
+   System.out.println(Float.SIZE/8);
+
+   //character can store the 2 bytes 
+   System.out.println(Character.SIZE/8);
+
+   //intger can store the 4 bytes 
+   System.out.println(Integer.SIZE/8);
+
+   //byte can store the 1 bytes 
+   System.out.println(Byte.SIZE/8);
+
+   //short can store the 2 bytes 
+   System.out.println(Short.SIZE/8);
+
+   //long can store the 8 bytes 
+   System.out.println(Long.SIZE/8);
+   
+   //double can store the 8 bytes 
+   System.out.println(Double.SIZE/8);
+   
+   //character can store the 2 bytes 
+   System.out.println(Character.SIZE/8);
+}
+}
