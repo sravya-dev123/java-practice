e2b1750e502ed3278e3df561c995343feeba3722
sravya sangadi <sravyasangadi@gmail.com>
2026-07-27 04:14:09 +0530

    realworld projects1
    today !!
    This Java program simulates a simple real-world shopping bill calculation. It stores the number of items and the cost of each item, calculates the total cost, and displays the bill with a currency symbol. This project demonstrates the use of variables, primitive data types, arithmetic operators, and output statements in Java.
    this small program output:
    Number of Items : 60
    Cost per Item   : 99.9$
    Total Cost      : 5994.0$

---
+++ realworldexample.java
@@ -0,0 +1,12 @@
+public class realworldexample{
+    public static void main(String[] args) {
+ int items = 60;
+ float itemcost = 99.9f;
+ float totalcost = items * itemcost;
+ char currency = '$';
+System.out.println("number of items:" + items);
+System.out.println("cost of the items:" + itemcost + currency);
+System.out.println("total cost items:" + totalcost + currency );
+
+    }
+}
