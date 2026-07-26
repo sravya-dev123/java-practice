ba46f8e02938624f4e2824453447041220ec8f8f
sravya sangadi <sravyasangadi@gmail.com>
2026-07-27 04:26:08 +0530

    real world project2
    today !!
    This Java program simulates a simple real-world tempurature. It stores the number of celius and the faranheit of eachtempurature, calculates the total tempurature, and displays the total tempurature of body . This project demonstrates the use of variables, primitive data types, arithmetic operators, and output statements in Java.
    this small program output:
    the tempurature celsius:30.0
    the fahrenheit tempurature:86.0

---
+++ realworldproject1.java
@@ -0,0 +1,8 @@
+public class realworldproject1 {
+    public static void main(String[] args) {
+        float celsius = 30;
+float fahrenheit = (celsius * 9/5) + 32;
+System.out.println("the tempurature celsius:" + celsius);
+System.out.println("the fahrenheit tempurature:" + fahrenheit);
+    }
+}
