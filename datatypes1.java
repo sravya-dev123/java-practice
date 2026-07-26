bcc25c6b6d8875a380261162acf55cd59c5c9e2b
sravya sangadi <sravyasangadi@gmail.com>
2026-07-27 04:11:42 +0530

    datatypes1
    small program by using int, char, float, etcc datatypes..

---
+++ datatypes1.java
@@ -0,0 +1,27 @@
+public class datatypes1 {
+    public static void main(String[] args) {
+        // primitive datatypes
+
+        //integer primitive datatypes
+        int id=25320-024;
+        System.out.println(id);
+
+        short height1=2;
+        System.out.println(height1);
+      
+        byte value=1;
+        System.out.println(value);
+
+        long height2=6;
+        System.out.println(height2);
+
+        
+        //decimal primitive datatypes
+        float markspercent=9.5f;
+        System.out.println(markspercent);
+
+        double sheet=4.5f;
+        System.out.println(sheet);
+    }
+    
+}
