cdcbab67328e8373a55b9a38b835c471e1e7e545
sravya sangadi <sravyasangadi@gmail.com>
2026-07-27 05:05:24 +0530

    datatype2
    This Java program stores and displays student information such as name, branch, age, and grade. It demonstrates the use of variables, primitive data types (`int`, `char`), and the non-primitive data type (`String`) in Java.
    the output:
    Student Name : Sravya
    Branch       : AI & ML
    Age          : 16
    Grade        : A

---
+++ datatypes2.java
@@ -0,0 +1,14 @@
+public class datatypes2 {
+    public static void main(String[] args) {
+
+        String name = "Sravya";
+        String branch = "AI & ML";
+        int age = 16;
+        char grade = 'A';
+
+        System.out.println("Student Name : " + name);
+        System.out.println("Branch       : " + branch);
+        System.out.println("Age          : " + age);
+        System.out.println("Grade        : " + grade);
+    }
+}
