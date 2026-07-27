17dbd454268c7cff673365b8a011cc0ed695da5a
sravya sangadi <sravyasangadi@gmail.com>
2026-07-27 20:56:47 +0530

    realtableformatee
    i am so exiting just am createing a new program by my self and exiciting and moving forwoard with my confidence..

---
+++ tableformatte.java
@@ -0,0 +1,36 @@
+public class tableformatte {
+    public static void main(String[] args) {
+        int id=101;
+        String course="cse";
+        String name="sravya";
+
+         int id1=102;
+        String course1="AI";
+        String name1="keerthi";
+
+         int id2=103;
+        String course2="ece";
+        String name2="monika";
+
+         int id3=104;
+        String course3="mech";
+        String name3="bhavya";
+
+         int id4=105;
+        String course4="civil";
+        String name4="sneha";
+
+        System.out.println("+----+--------+--------+");
+        System.out.println("| ID | course | name   |");
+        System.out.println("+----+--------+--------+");
+        System.out.println("| " + id  + " | " + course  + "   | " + name  + " |");
+        System.out.println("+----+--------+--------+");
+        System.out.println("| " + id1 + " | " + course1 + "    | " + name1 + "|");
+        System.out.println("+----+--------+--------+");
+        System.out.println("| " + id2 + " | " + course2 + "   | " + name2 + " |");
+        System.out.println("+----+--------+--------+");
+        System.out.println("| " + id3 + " | " + course3 + "  | " + name3 + " |");
+        System.out.println("+----+--------+--------+");
+        System.out.println("| " + id4 + " | " + course4 + " | " + name4 + "  |");
+    }
+}
