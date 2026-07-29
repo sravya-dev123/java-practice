e593674bb477898edcdf686a34336d2ca76c216a
sravya sangadi <sravyasangadi@gmail.com>
2026-07-29 19:49:49 +0530

    method syntax
    today i learned java methods..
    Java Methods: Examples of method declaration, parameters, return types, and method calls.

---
+++ methods.java
@@ -0,0 +1,9 @@
+public class methods{//class
+    public static void greets() {// public-access specifier, static fun,void no-return type, greets-method name
+        System.out.println("hello world");//method print
+    }
+public static void main(String[] args){//main function
+   greets();//method call fun
+
+}
+    }
