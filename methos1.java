55bc93778c94260f690beebba34ac302734eb6b6
sravya sangadi <sravyasangadi@gmail.com>
2026-07-31 07:33:18 +0530

    methods small program
    i create small program by using methods 
    output of this program is :
    hello world

---
+++ methos1.java
@@ -0,0 +1,11 @@
+public class methos1 {
+    public static void greets() {
+        System.out.println("hello world");
+    }
+    
+
+public static void main(String[] args){
+   greets();
+}
+    }
+
