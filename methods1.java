4b72a08e058f4f78794241ca70f88ae1c78cff40
sravya sangadi <sravyasangadi@gmail.com>
2026-08-01 19:02:35 +0530

    methods 1
    today i created small method program by using the login function ,by passing parameters like username and password 
    output:
    username:sravya
    password:sravya1
    login successfully completed

---
+++ methods1.java
@@ -0,0 +1,13 @@
+public class methods3 {
+    static void  login(String username,String password) {
+        System.out.println("username:"+ username);
+        System.out.println("password:"+ password);
+System.out.println("login successfully completed");
+    }
+    public static void main(String[] args){
+
+        String username="sravya";
+        String password="sravya1";
+        login(username,password);
+    }
+}
