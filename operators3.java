29a7a3f1dedff2e7ba5245b8ed8305afea8f2840
sravya sangadi <sravyasangadi@gmail.com>
2026-08-02 15:33:15 +0530

    operator /
    output:
    calculation:4
    calculation:99
    calculation:933
    calculation:499
    calculation:40
    calculation:40
    calculation:99-392
    calculation:9330
    calculation:00

---
+++ operators3.java
@@ -0,0 +1,30 @@
+public class operators3 {
+          public static void main(String[] args) 
+    {
+
+ int x = 33567/ 7356;
+ int y = 382755 / 3852;
+ int z = 3382623/ 3625; 
+ int x1 = x / 5663;
+ int y1 = x / y - 392;
+ int z1 = x / y / z / 3653;
+
+ System.out.println("calculation:" + x );
+ System.out.println("calculation:" + y );
+ System.out.println("calculation:" + z );
+ System.out.println("calculation:" + x + y );
+ System.out.println("calculation:" + x +( y / z) );
+ System.out.println("calculation:" + x + x1 );
+ System.out.println("calculation:" + y + y1 );
+ System.out.println("calculation:" + z + z1 );
+ System.out.println("calculation:" + (x1 / y1) + z1);
+ System.out.println("calculation:" +( x / y / z) + (x1 / y1 / z1));
+ System.out.println("calculation:" + (x / y) );
+ System.out.println("calculation:" + (x / y) + z );
+ System.out.println("calculation:" + x + x1 );
+ System.out.println("calculation:" + y + y1 );
+ System.out.println("calculation:" + z + z1 );
+ System.out.println("calculation:" + x1 +( y1 / z1));
+ System.out.println("calculation:" +( x / y / z )+( x1 / y1 / z1));
+    }
+}
