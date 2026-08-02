53e6e3cea72e6db0ccbd0ec3fa6846bfa98dd87c
sravya sangadi <sravyasangadi@gmail.com>
2026-08-02 23:24:15 +0530

    arithmatic operators
    today i create small program by using all arthimatic operators like
     + addition
     - subtraction
     * multiplication
     / divison
     % module

---
+++ arthmaticoperator.java
@@ -0,0 +1,21 @@
+public class arthmaticoperator {
+    public static void main(String[] args){
+    
+        int x = 10;
+        int y = 30;
+
+        System.out.println(x + y);
+         System.out.println(x - y);
+          System.out.println(x * y);
+           System.out.println(x / y);
+            System.out.println(x % y);
+
+            int z = 2;
+            z++;
+            System.out.println(z);
+            int z1 = 4;
+            z1--;
+             System.out.println(z1);
+
+    }
+}
