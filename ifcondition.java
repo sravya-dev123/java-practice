e41b512b65b0e096976ccbfea7690a6524b65889
sravya sangadi <sravyasangadi@gmail.com>
2026-08-21 18:24:33 +0530

    if condition
    📚 Day by Day Learning Java!
    
    Implemented an **If Condition** program to understand decision-making in Java. Every small step in coding brings me closer to my goals.

---
+++ ifcondition.java
@@ -0,0 +1,13 @@
+public class ifcondition {
+
+    public static void main(String[] args) {
+        String name1="sravya";
+        String name2="sneha";
+        if(name1.length()>name2.length()){
+            System.out.println("sneha word is big");
+    }
+    else{
+        System.out.println("sravya word is big");
+    }
+}
+}
