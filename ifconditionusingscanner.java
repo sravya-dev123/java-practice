c6b56aa2b9259d111545043f2daf7eb837c09103
sravya sangadi <sravyasangadi@gmail.com>
2026-08-21 18:38:47 +0530

    if condition scanner
    if condition using scanner
    📚 Day by Day Learning Java!
    
    Implemented an **If Condition** program to understand decision-making in Java. Every small step in coding brings me closer to my goals.

---
+++ ifconditionusingscanner.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+public class ifconditionusingscanner {
+    public static void main(String[] args) {
+        Scanner Sc=new Scanner(System.in);
+        int i=Sc.nextInt();
+        if(i<0){
+            System.out.println("this is negative");
+        }
+    }
+}
+
