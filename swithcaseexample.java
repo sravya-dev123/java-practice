f198e1b040cf89a5b2b5a9f6846307d47726196c
sravya sangadi <sravyasangadi@gmail.com>
2026-08-24 21:19:07 +0530

    switch case statement

---
+++ swithcaseexample.java
@@ -0,0 +1,40 @@
+
+import java.util.Scanner;
+public class swithcaseexample{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a number:");
+int i = sc.nextInt();
+        switch(i){
+case 1:
+    System.out.println("monday");
+break;
+
+case 2:
+    System.out.println("tuesday");
+  break;
+    case 3:
+        System.out.println("wednesday");
+break;
+
+        case 4:
+    System.out.println("thursday");
+break;
+
+case 5:
+    System.out.println("friday");
+break;
+
+    case 6:
+        System.out.println("saturday");
+break;
+
+        case 7:
+            System.out.println("sunday");
+            break;
+
+            default :
+            System.out.println("invalid day");
+          }
+        }
+    }
