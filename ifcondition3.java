4b2d84b8f7aa294b4edf69b9995e7184cad30efd
sravya sangadi <sravyasangadi@gmail.com>
2026-08-24 06:46:23 +0530

    if condition
    if condition by using == operator

---
+++ ifcondition3.java
@@ -0,0 +1,10 @@
+public class ifcondition3 {
+        public static void main(String[] args) {
+        int x=30;
+        int y=30;
+        if(x==y){
+            System.out.println("y is equal to x");
+        }
+    }
+}
+
