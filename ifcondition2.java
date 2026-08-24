cef50a4054441fca0f9e95f18e6134a70b0ff3e7
sravya sangadi <sravyasangadi@gmail.com>
2026-08-24 06:44:07 +0530

    if condition

---
+++ ifcondition2.java
@@ -0,0 +1,9 @@
+public class ifcondition2 {
+    public static void main(String[] args) {
+        int x=30;
+        int y=43;
+        if(x<y){
+            System.out.println("y is the greater than x");
+        }
+    }
+}
