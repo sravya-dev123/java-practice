3590cc9b3a668c59df31fcb4283ec6bc4d3dcdd0
sravya sangadi <sravyasangadi@gmail.com>
2026-08-24 06:49:05 +0530

    if condition
    if condition by using boolean operation

---
+++ ifcondition4.java
@@ -0,0 +1,9 @@
+public class ifcondition4 {
+        public static void main(String[] args) {
+        boolean light=true;
+        if(light){
+            System.out.println("light on is true");
+        }
+    }
+}
+
