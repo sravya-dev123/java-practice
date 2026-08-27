a34e930aa9968b5fdd40a6f2733a4fa1ec1d8eed
sravya sangadi <sravyasangadi@gmail.com>
2026-08-27 20:59:54 +0530

    constructor
    today i learned about the how to return or called the contsruction

---
+++ constructor2.java
@@ -0,0 +1,10 @@
+public class constructor2 {
+    int x;
+    public constructor2(){
+        x=86;
+    }
+    public static void main(String[] args) {
+        constructor2 my=new constructor2();
+        System.out.println(my.x);
+    }
+}
