8e1af29f222f62f1d3407aba36b2030c3e4d41c5
sravya sangadi <sravyasangadi@gmail.com>
2026-08-27 21:02:59 +0530

    constructor

---
+++ constructor3.java
@@ -0,0 +1,10 @@
+public class constructor3 {
+    int x;
+    public constructor3(int y){
+        x=y;
+    }
+    public static void main(String[] args) {
+        constructor3 main=new constructor3(50);
+        System.out.println(main.x);
+    }
+}
