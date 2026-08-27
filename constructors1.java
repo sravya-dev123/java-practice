5a41428a2c6f9c52c3ab64bdb284b5717decd458
sravya sangadi <sravyasangadi@gmail.com>
2026-08-27 20:54:17 +0530

    constructor

---
+++ constructors1.java
@@ -0,0 +1,13 @@
+public class constructors1 {
+   public int age;
+   public String name;
+       constructors1(){
+name="sravya";
+age=17;
+       }
+    public static void main(String[] args) {
+        constructors1 cp=new constructors1();
+System.out.println(cp.name);
+System.out.println(cp.age);
+    }
+    }
