6561b3c1303c9edb42814e9452c7f890fdc105b6
sravya sangadi <sravyasangadi@gmail.com>
2026-08-28 19:47:21 +0530

    object
     Java Learning 🚀
    
    I am learning Java programming step by step and practicing different concepts through simple programs.
    
     Topics I am learning:
    
    * Variables & Data Types
    * Operators
    * Conditional Statements
    * Loops
    * Arrays
    * Methods
    * Constructors
    * OOP Concepts
    * Inheritance
    * Method Overloading & Overriding
    * Interfaces
    * Exception Handling
    
    This repository contains my Java practice programs and learning progress. 📚💻

---
+++ object2.java
@@ -0,0 +1,9 @@
+public class object2 {
+    int x=5;
+public static void main(String[] args) {
+    object2 main=new object2();
+    object2 main1=new object2();
+    System.out.println(main1.x);
+    System.out.println(main.x);
+}
+}
