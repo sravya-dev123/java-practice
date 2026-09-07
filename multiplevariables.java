0a8177bdef3ff77f443293fc8653e13d6b0f5e8b
sravya sangadi <sravyasangadi@gmail.com>
2026-09-07 07:06:10 +0530

    multiple variables

---
+++ multiplevariables.java
@@ -0,0 +1,17 @@
+public class multiplevariables {
+    public static void main(String[] args) {
+        //multiple variables
+        int x=3;
+        int y=2;
+        int z=8;
+        System.out.println("print:"+x+y+z);//o/p is 328
+        System.out.println("print:"+(x+y+z));// output is 13
+        //different style
+        int nam1=7, nam2=22 , nam3=27;
+        System.out.println("different style:"+nam1+nam2+nam3);// output = 72227
+        //one value to multiple variables
+       int x1, y1, z1;
+x1 = y1 = z1 = 50;
+System.out.println(x1 + y1 + z1); // 150
+    }
+}
