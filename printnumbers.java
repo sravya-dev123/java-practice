10fbd1b59ab74fc92314b3f8777f3d4eb0fb01ef
sravya sangadi <sravyasangadi@gmail.com>
2026-09-07 06:35:17 +0530

    print numbers

---
+++ printnumbers.java
@@ -0,0 +1,11 @@
+public class printnumbers {
+        /*You can add as many println() methods as you want. Note that it will add a new line for each method: */
+public static void main(String[] args) {
+    System.out.println("27");
+/*Text must be wrapped inside double quotations marks "". */
+/*difference between print satement and println statements */
+System.out.print(200);
+System.out.println(17+872);/*i doesnot print a new line soo it can automattically print it self by line */
+/*numbers cannot print double queaotations */
+}
+}
