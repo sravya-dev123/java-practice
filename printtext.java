d174e96ba59c9f82213881bbda4bb206db03126e
sravya sangadi <sravyasangadi@gmail.com>
2026-09-07 06:31:59 +0530

    print text like print , println

---
+++ printtext.java
@@ -0,0 +1,10 @@
+public class printtext {
+    /*You can add as many println() methods as you want. Note that it will add a new line for each method: */
+public static void main(String[] args) {
+    System.out.println("hello");
+/*Text must be wrapped inside double quotations marks "". */
+/*difference between print satement and println statements */
+System.out.print("hyy gugs");
+System.out.println("welcome");/*i doesnot print a new line soo it can automattically print it self by line */
+}
+}
