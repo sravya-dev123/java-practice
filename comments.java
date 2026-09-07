d5d1a339b15c626577d65b4126d38c00494e86ec
sravya sangadi <sravyasangadi@gmail.com>
2026-09-07 06:39:28 +0530

    single & multi line comments

---
+++ comments.java
@@ -0,0 +1,12 @@
+public class comments {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        // this is a single-line comments
+        //Any text between // and the end of the line is ignored by Java (will not be executed).
+
+        System.out.println("hello");
+/*multi-line comments 
+Multi-line comments start with /* and ends with arstric symbol plus /.*/
+
+    }
+}
