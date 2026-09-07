5eb0d6e938245438b7d5f62b597f8845f0726e20
sravya sangadi <sravyasangadi@gmail.com>
2026-09-07 06:24:43 +0530

    syntax

--- syntax.java
+++ syntax.java
@@ -1,5 +1,9 @@
-class Syntax{
-    public static void main(String[] args){
-        System.out.println("this is main syntax");
-    }
+public class syntax {
+    /*Every line of code that runs in Java must be inside a class. The class name should always start with an uppercase first letter. In our example, we named the class Main. */
+    public static void main(String[] args) {
+        /*The main() method is required in every Java program. It is where the program starts running: */
+   /*For now, you don't need to understand the keywords public, static, and void. You will learn about them later in this tutorial. Just remember: main() is the starting point of every Java program. */
+  System.out.println("hello world");
+/*Inside the main() method, we can use the println() method to print a line of text to the screen:*/
+}
 }
