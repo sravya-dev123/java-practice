6d9d1f581394ef8e289a4fae92f6314a8fb16808
sravya sangadi <sravyasangadi@gmail.com>
2026-09-09 06:37:03 +0530

    identifiesrs
    Names can contain letters, digits, underscores, and dollar signs
    Names must begin with a letter
    Names should start with a lowercase letter, and cannot contain whitespace
    Names can also begin with $ and _
    Names are case-sensitive ("myVar" and "myvar" are different variables)
    Reserved words (like Java keywords, such as int or boolean) cannot be used as names

---
+++ identifiers.java
@@ -0,0 +1,20 @@
+public class identifiers {
+    /*All Java variables must be identified with unique names.
+
+These unique names are called identifiers.*/
+
+// rules 
+
+
+/*Names can contain letters, digits, underscores, and dollar signs
+Names must begin with a letter
+Names should start with a lowercase letter, and cannot contain whitespace
+Names can also begin with $ and _
+Names are case-sensitive ("myVar" and "myvar" are different variables)
+Reserved words (like Java keywords, such as int or boolean) cannot be used as names */
+
+public static void main(String[] args) {
+    int myVar=60;
+    System.out.println(myVar);
+}
+}
