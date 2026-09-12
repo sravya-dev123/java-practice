59b2ab0ddf678e7c0c78f1e2a28e8c992801987a
sravya sangadi <sravyasangadi@gmail.com>
2026-09-12 20:31:57 +0530

    primitive datatypes

---
+++ primitive1.java
@@ -0,0 +1,41 @@
+public class primitive1 {
+    public static void main(String[] args) {
+        /*
+        boolean:
+        Very often in programming, you will need a data type that can only have one of two values, like:
+
+YES / NO
+ON / OFF
+TRUE / FALSE
+        */
+
+boolean yes = true;
+boolean no = false;
+System.out.println(yes);    
+System.out.println(no);
+
+
+/*
+Characters
+The char data type is used to store a single character. The character must be surrounded by single quotes, like 'A' or 'c':
+*/
+
+char Grade = 'B';
+System.out.println(Grade);
+
+//diif ways
+char Var1 = 65, Var2 = 66, Var3 = 67;
+System.out.println(Var1);
+System.out.println(Var2);
+System.out.println(Var3);
+
+
+/*string
+The String data type is used to store a sequence of characters (text). String values must be surrounded by double quotes:
+*/
+
+String greeting = "Hello World";
+System.out.println(greeting);
+
+    }
+}
