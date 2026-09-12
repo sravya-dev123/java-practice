1d15fc20ca316dac6dd6bd8dafdad32f16709e73
sravya sangadi <sravyasangadi@gmail.com>
2026-09-12 20:01:34 +0530

    real-life-example
    studentname:sravya
    studentID:101
    studentAGE:17
    studentFEES:1000.5
    studentGRADE:A

---
+++ RealLifeExample.java
@@ -0,0 +1,16 @@
+public class RealLifeExample {
+    public static void main(String[] args){
+        String student="sravya";
+        int stdid=101;
+        int stdage=17;
+      float stdfee=1000.50f;
+         char stdgrade='A';
+
+     System.out.println("studentname:"+student);
+     System.out.println("studentID:"+stdid);
+     System.out.println("studentAGE:"+stdage);
+     System.out.println("studentFEES:"+stdfee);
+     System.out.println("studentGRADE:"+stdgrade);
+    
+    }
+}
