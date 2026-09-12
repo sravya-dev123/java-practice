19eaf94a4052834174417f860756c3088d24ea82
sravya sangadi <sravyasangadi@gmail.com>
2026-09-12 20:05:54 +0530

    calculate area of rectangle
    In this real-life example, we create a program to calculate the area of a rectangle (by multiplying the length and width):

---
+++ RealLifeExample1.java
@@ -0,0 +1,13 @@
+public class RealLifeExample1 {
+ public static void main(String[] args) {
+    int length=846;
+    int width=475;
+    int area;
+
+    area=length*width;
+
+    System.out.println("length:"+length);
+    System.out.println("width:"+width);
+    System.out.println("area:"+area);
+ }   
+}
