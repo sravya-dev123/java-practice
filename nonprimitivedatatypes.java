72db4fe9e1e9e2a42505ac4cc4aa3e020538c084
sravya sangadi <sravyasangadi@gmail.com>
2026-09-12 20:34:50 +0530

    non-primitive datatypes

---
+++ nonprimitivedatatypes.java
@@ -0,0 +1,29 @@
+public class nonprimitivedatatypes {
+    public static void main(String[] args) {
+        
+
+        /*non premitive dattaypes means
+        
+        array
+        string
+        classes
+        etc..*/
+        //i'm not go that advanced studies
+
+        /*
+        so bacically i learned about it
+        */
+
+        /*
+        Non-Primitive Data Types
+Non-primitive data types are called reference types because they refer to objects.
+
+The main differences between primitive and non-primitive data types are:
+
+Primitive types in Java are predefined and built into the language, while non-primitive types are created by the programmer (except for String).
+Non-primitive types can be used to call methods to perform certain operations, whereas primitive types cannot.
+Primitive types start with a lowercase letter (like int), while non-primitive types typically starts with an uppercase letter (like String).
+Primitive types always hold a value, whereas non-primitive types can be null.
+        */
+    }
+}
