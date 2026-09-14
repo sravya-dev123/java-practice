541f1b5c59ee54834344d6d0260b3ef28038d4f1
sravya sangadi <sravyasangadi@gmail.com>
2026-09-14 11:53:04 +0530

    do-while loop

---
+++ looping2.java
@@ -0,0 +1,18 @@
+public class looping2 {
+    /*
+    do{
+    code
+    }while(condtion);
+     */
+
+    //first we gave the  condition and then we will check the while the condition become true or a false
+
+    public static void main(String[] args) {
+        int i=1;
+        do{
+System.out.println(i);
+i++;
+        }
+        while(i<10);
+    }
+}
