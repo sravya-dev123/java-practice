2022d820a682a387380fbdeb827ccbf5e7ca7773
sravya sangadi <sravyasangadi@gmail.com>
2026-09-14 11:58:18 +0530

    do-while with false statements

---
+++ looping4.java
@@ -0,0 +1,19 @@
+public class looping4 {
+    /*syntax
+    do {
+  // code block to be executed
+}
+while (condition);*/
+public static void main(String[] args) {
+    //looping with flase statemnts
+
+    int i = 10;
+do {
+  System.out.println(i);
+  i++;
+}
+while (i < 5);
+}
+}
+
+//this staements is false but this is print 10 so do can print only one statements
