9b3b41e6d942764c0c023bf3e76b97692eab2f36
sravya sangadi <sravyasangadi@gmail.com>
2026-09-14 11:46:50 +0530

    while loop

---
+++ looping1.java
@@ -0,0 +1,20 @@
+public class looping1 {
+    //while statements
+    /*while syntax:
+    while (condion){
+code
+    }*/
+
+//looping statments are execute several times
+
+
+public static void main(String[] args) {
+    int i=8;
+    while(i<10){
+System.out.println(i);
+i++;
+    }
+}
+}
+
+
