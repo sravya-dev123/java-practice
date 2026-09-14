ad70fb8798ff9bdb27665bff6ccb68b8a605fe5f
sravya sangadi <sravyasangadi@gmail.com>
2026-09-14 11:47:32 +0530

    while loop with false statement

---
+++ looping3.java
@@ -0,0 +1,10 @@
+public class looping3 {
+    public static void main(String[] args) {
+        int i=10;
+        while(i<5){
+             System.out.println("this will never be printed");
+            i++;
+           
+        }
+    }
+}
