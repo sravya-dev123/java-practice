5c5ca700ad0389798ddad4abbf46091bac003960
sravya sangadi <sravyasangadi@gmail.com>
2026-09-15 22:42:42 +0530

    for loop with flase condition

---
+++ looping8.java
@@ -0,0 +1,9 @@
+public class looping8 {
+    public static void main(String[] args) {
+        
+    
+        for (int i = 10; i < 5; i++) {
+      System.out.println("This will never be printed");
+    }
+  }
+}
