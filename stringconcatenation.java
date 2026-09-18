ab3afcd7a16f52058db2e6ec4c0be135c5cc2522
sravya sangadi <sravyasangadi@gmail.com>
2026-09-18 20:15:53 +0530

    string concatination

---
+++ stringconcatenation.java
@@ -0,0 +1,12 @@
+public class stringconcatenation {
+    
+    public static void main(String[] args) {
+        String fristname="sravya";
+        String lastname="sangadi";
+        String fullname=fristname + " "+lastname;
+
+
+        System.out.println("fullname:"+ fullname);
+
+    }
+}
